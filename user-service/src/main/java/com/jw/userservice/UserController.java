702004125dfbbdc8bbadd44ea0ff90ac29e5528c
kimjusung <dlrtls12345@naver.com>
2022-05-05 21:05:15 +0900

    login 기능구현

--- user-service/src/main/java/com/jw/userservice/UserController.java
+++ user-service/src/main/java/com/jw/userservice/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.jw.userservice.UserDto.*;
@@ -15,15 +16,16 @@ public class UserController
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(UserRegisterRequestDto requestDto)
+    public ResponseEntity<String> register(@RequestBody UserRegisterRequestDto requestDto)
     {
         String email = userService.register(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(email);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login()
+    public ResponseEntity<Boolean> login(@RequestBody UserLoginRequestDto requestDto)
     {
-
+        Boolean result = userService.login(requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
