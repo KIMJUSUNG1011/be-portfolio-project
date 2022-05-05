a166da20fa7804494b23df5a66df52bb2a81c115
kimjusung <dlrtls12345@naver.com>
2022-05-05 21:57:16 +0900

    login test end

--- user-service/src/test/java/com/jw/userservice/UserServiceTest.java
+++ user-service/src/test/java/com/jw/userservice/UserServiceTest.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static com.jw.userservice.UserDto.UserRegisterRequestDto;
 
+import com.jw.userservice.UserDto.*;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
@@ -34,5 +36,18 @@ class UserServiceTest
     @Test
     void login()
     {
+        // given : 이런 환경일 때(미리 선언)
+        String email = "email";
+        String password = "12345";
+        UserRegisterRequestDto userRegisterRequestDto = new UserRegisterRequestDto("email", "12345", "이름", "01012345678");
+        userService.register(userRegisterRequestDto);
+
+        // when : 이런 것을 하면(호출)
+        UserLoginRequestDto requestDto = new UserLoginRequestDto(email, password);
+
+        // then : 결과
+        Boolean result = userService.login(requestDto);
+
+        Assertions.assertThat(result).isEqualTo(true);
     }
 }
