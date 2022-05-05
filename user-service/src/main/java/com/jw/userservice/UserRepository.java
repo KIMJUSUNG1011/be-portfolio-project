4dab381e6db755cf125a5b78c2c63abde35e9467
WOOSERK <syeokim@naver.com>
2022-05-05 19:57:12 +0900

    user-service 구현

---
+++ user-service/src/main/java/com/jw/userservice/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jw.userservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>
+{
+}
---
+++ user-service/src/main/java/com/jw/userservice/UserService.java
@@ -0,0 +1,8 @@
+package com.jw.userservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService
+{
+}
