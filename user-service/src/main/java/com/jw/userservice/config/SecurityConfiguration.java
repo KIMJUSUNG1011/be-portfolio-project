ee7bc31df68e37ed844ca760e03a20ce36cd30ec
WOOSERK <syeokim@naver.com>
2022-05-08 19:51:14 +0900

    Security 코드 수정

--- user-service/src/main/java/com/jw/userservice/config/SecurityConfiguration.java
+++ user-service/src/main/java/com/jw/userservice/config/SecurityConfiguration.java
@@ -2,10 +2,13 @@ package com.jw.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,25 +20,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity httpSecurity) throws Exception
     {
         httpSecurity
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/register")
-                .permitAll()
-
-                .and()
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/h2-console/**")
-                .permitAll()
-
-                .and()
-                .csrf()
-                .ignoringAntMatchers("/h2-console/**")
-                .disable();
+                .csrf().disable();
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception
+    public void configure(WebSecurity web)
     {
         web.ignoring().antMatchers("/h2-console/**");
     }
