b876e6e2eb1da74afc594b327121d439a600f40e
WOOSERK <syeokim@naver.com>
2022-05-14 23:19:56 +0900

    Feat: config 서비스 구현

---
+++ config-service/src/test/java/com/jw/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jw.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
