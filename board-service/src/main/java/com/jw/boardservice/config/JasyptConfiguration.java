de4bbba6c49e2433526aa0403a2d935a824e9956
WOOSERK <syeokim@naver.com>
2022-05-14 23:17:00 +0900

    Feat: config 서비스 생성

--- board-service/src/main/java/com/jw/boardservice/config/JasyptConfiguration.java
+++ board-service/src/main/java/com/jw/boardservice/config/JasyptConfiguration.java
@@ -11,18 +11,17 @@ import org.springframework.context.annotation.Configuration;
 public class JasyptConfiguration
 {
     @Value("${jasypt.encryptor.password}")
-    private String key;
+    private String PASSWORD;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor()
     {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(key);
+        config.setPassword(PASSWORD);
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
-        config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
         config.setStringOutputType("base64");
         encryptor.setConfig(config);
