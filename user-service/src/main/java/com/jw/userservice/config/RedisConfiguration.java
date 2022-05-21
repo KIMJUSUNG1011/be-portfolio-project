800f617979b05f99b4f9164127ebe929d4bfda6f
WOOSERK <syeokim@naver.com>
2022-05-22 01:47:39 +0900

    Feat: 세션 리스너 구현

--- user-service/src/main/java/com/jw/userservice/config/RedisConfiguration.java
+++ user-service/src/main/java/com/jw/userservice/config/RedisConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
-@EnableRedisHttpSession
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 10)
 @Configuration
 public class RedisConfiguration extends AbstractHttpSessionApplicationInitializer
 {
