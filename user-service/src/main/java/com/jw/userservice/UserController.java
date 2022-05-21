b5e1c083eb89fff36ebe6ca8aea3962fd40e9de7
kimjusung <dlrtls12345@naver.com>
2022-05-21 23:29:08 +0900

    FIX : merge by jusung

--- user-service/src/main/java/com/jw/userservice/UserController.java
+++ user-service/src/main/java/com/jw/userservice/UserController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
